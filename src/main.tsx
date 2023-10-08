049ad8be321cad7f554955fde61fad5ca3dc5d88
iluhaar <iluhaar@gmail.com>
2023-10-08 14:00:03 +0300

    fix pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, HashRouter, RouterProvider } from "react-router-dom";
 import Encoder from "./components/Encoder/Encoder.tsx";
 import Layout from "./components/Layout/Layout.tsx";
 import { MarkdownEditor } from "./components/Editor/MarkdownEditor.tsx";
@@ -9,23 +9,23 @@ import App from "./App.tsx";
 
 const router = createBrowserRouter([
   {
-    path: "/who-am-i/",
+    path: "/",
     element: <Layout />,
     children: [
       {
-        path: "/who-am-i/",
+        path: "/",
         element: <App />,
       },
       {
-        path: "/who-am-i/tools/encoder",
+        path: "/tools/encoder",
         element: <Encoder />,
       },
       {
-        path: "/who-am-i/tools/editor",
+        path: "/tools/editor",
         element: <MarkdownEditor />,
       },
       {
-        path: "/who-am-i/tools/note/:id",
+        path: "/tools/note/:id",
         element: <MarkdownEditor />,
       },
     ],
@@ -34,6 +34,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <RouterProvider router={router} />
+    </HashRouter>
   </React.StrictMode>
 );
